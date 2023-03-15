ed9d62b0baff4470ee7db4f5a2bef14ab6291a00
Mathieu GRUSON <mathieu.gruson@gmail.com>
2023-03-15 17:44:04 +0100

    use of epoll to have several connections

--- merge.cpp
+++ merge.cpp
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 15:39:03 by mgruson           #+#    #+#             */
-/*   Updated: 2023/03/15 16:26:34 by mgruson          ###   ########.fr       */
+/*   Updated: 2023/03/15 17:43:33 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@
 
 #define MAX_EVENTS 5
 #define READ_SIZE 1000
-#define PORT 8081
+#define PORT 8082
 
-int main(int argc, char const *argv[])
+
+
+int getServerFd()
 {
-	int		new_socket;
-	long	valread;
 
 	/*
 	The sockaddr_in struct is a part of the BSD sockets API in Linux and is used to specify internet addresses.
@@ -41,15 +41,17 @@ int main(int argc, char const *argv[])
 	sin_addr: Specifies the IP address for the socket.
 	sin_zero: Padding field.
 	*/
+	int		new_socket;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
-	std::string answer = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+
 	/*
 	Socket() : 
 		Type of IP : AF_INET for IPv4 (AF_INET6 for IPv6 addresses)
 		Type of protocol to exchance (SOCK_STREAM for TCP) and the protocol (0 for default protocol).
 	Therefore, server_fd = socket(AF_INET, SOCK_STREAM, 0) creates a new socket file descriptor for IPv4 TCP connections.
 	*/
+
 	int server_fd;
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
@@ -67,7 +69,7 @@ int main(int argc, char const *argv[])
 	/* htons() stands for "host-to-network short", and it converts a 16-bit unsigned integer from host-byte-order to network-byte-order. This function is typically required by most networking functions that deal with port numbers since different computer architectures might have different byte-orders (endianness).*/
 	address.sin_port = htons( PORT );
 	memset(address.sin_zero, '\0', sizeof address.sin_zero);
-	
+
 	/*
 	The bind() function binds a socket to a specific address and port number.
 	It takes three arguments:
@@ -76,14 +78,12 @@ int main(int argc, char const *argv[])
 	- the third one is the size of the struct sockaddr object.
 	In this case, the bind() function is binding the server_fd socket to the address and port number defined in address. The sizeof(address) is passed as the third argument to specify the size of the struct sockaddr object passed in.
 	*/
+
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
 	{
 		perror("In bind");
 		exit(EXIT_FAILURE);
 	}
-
-	/*-------------------------------------------------*/
-
 	/*
 	The listen() function allows the program to listen on the socket for incoming connections.
 	The first argument is the socket file descriptor returned by socket().
@@ -94,12 +94,25 @@ int main(int argc, char const *argv[])
 		perror("In listen");
 		exit(EXIT_FAILURE);
 	}
-	std::cout << "Waiting for new connection" << std::endl;
+	/*
+	accept() is a system call that accepts connections on the socket, it extracts one connection from the queue of pending connections of the listening socket.
+	The first parameter in this function call is the socket file descriptor returned by socket(). The second argument points to a buffer where the address of the connecting client should be stored. And the last argument is set to the size of the sockaddr_in structure pointed to by the second argument.
+	The accept() function returns another file descriptor(new_socket) for the newly created TCP socket created between the server and the client, through which the server will communicate with the client.
+	*/
+	std::cout << "Waiting for acception" << std::endl;
 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
 	{
 		perror("In accept");
 		exit(EXIT_FAILURE);
 	}
+	return (new_socket);
+}
+
+int	start_server(int new_socket)
+{
+long	valread;
+
+	std::string answer = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 	size_t bytes_read;
 	char read_buffer[READ_SIZE + 1];
 	struct epoll_event event, events[MAX_EVENTS];
@@ -117,7 +130,7 @@ int main(int argc, char const *argv[])
 	}
 	int event_count = 0;
 	while (1) {
-		std::cout << "Polling for input..." << std::endl;
+		std::cout << "Waiting for new request" << std::endl;
 		event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
 		std::cout << "Polling for input..." << event_count << std::endl;
 		std::cout << event_count << " ready_event" << std::endl;
@@ -125,12 +138,26 @@ int main(int argc, char const *argv[])
 			std::cout << "Reading file descriptor " << events[i].data.fd << std::endl;
 			int bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
 			read_buffer[bytes_read] = '\0';
-			printf("Read '%s'\n", read_buffer);
+			std::cout << read_buffer << std::endl;
+			write(new_socket , answer.c_str() , strlen(answer.c_str()));
 		}
 	}
 	if (close(epoll_fd)) {
 		fprintf(stderr, "Failed to close epoll file descriptor\n");
 		return 1;
-	}
+	}	
+}
+
+int main(int argc, char const *argv[])
+{
+	
+
+	int new_socket = getServerFd();
+
+	start_server(new_socket);
+	
+	// if (close(server_fd) == -1) {
+	// 	perror("Failed to close server socket file descriptor");
+	// }
 	return 0;
 }
