0e82e16256cb6ce17838e1781b86e24f3b592dc5
Nicolas FLAN <nflan@e3r12p16.42.fr>
2023-03-29 19:04:51 +0200

    oups je suis sur le main, debut error page

--- objects/main.d
+++
@@ -1,8 +0,0 @@
-objects/main.o: sources/main.cpp sources/server_configuration.hpp \
-  sources/server_response.hpp sources/server_request.hpp
-
-sources/server_configuration.hpp:
-
-sources/server_response.hpp:
-
-sources/server_request.hpp:
