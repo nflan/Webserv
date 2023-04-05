1afb428c407226d8a14a3906b3497cfd2230cd51
BekxFR <104771705+BekxFR@users.noreply.github.com>
2023-04-05 19:04:19 +0200

    Parsing request 80%

--- objects/server_configuration.d
+++
@@ -1,6 +0,0 @@
-objects/server_configuration.o: sources/server_configuration.cpp \
-  sources/server_configuration.hpp sources/default_error.hpp
-
-sources/server_configuration.hpp:
-
-sources/default_error.hpp:
